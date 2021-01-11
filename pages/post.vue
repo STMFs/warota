08e525b5d8236f3bfe169914cfacf21ab553741e
k18083kk <k18083kk@pck18083kk.local>
2021-01-11 22:20:57 +0900

    お題投稿

---
+++ pages/post.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h1>お題投稿</h1>
+    <form @submit="$event.preventDefault(), themePost()">
+      <input v-model="value" required />
+      <button type="submit">投稿</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import firebase from "@/plugins/firebase.js";
+
+export default {
+  data() {
+    const value = "";
+    return {
+      value
+    };
+  },
+  methods: {
+    themePost() {
+      console.log(this.$data.value);
+
+      const themeData = {
+        content: this.value
+      };
+      firebase
+        .firestore()
+        .collection("theme")
+        .doc()
+        .set(themeData)
+        .then(() => {
+          this.value = ""; // input要素の値を空白に
+        });
+    }
+  }
+};
+</script>
+
+<style></style>
