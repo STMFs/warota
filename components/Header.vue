3b8c2f9cffb6832677e63796943539986b45a92b
sugihara10969 <59116409+sugihara10969@users.noreply.github.com>
2021-01-15 22:26:50 +0900

    Fix Headerのcssが適応されないのを修正

--- components/Header.vue
+++ components/Header.vue
@@ -1,10 +1,10 @@
 <template>
     <div>
-        <header>
-            <div class="title">worota</div>
-            <div class="buttons">
-              <button class="hbutton" id="login_button" @click="Login()">ログイン</button>
-              <button class="hbutton" @click="Route_post()">お題投稿</button>
+        <header :class="$style.header">
+            <div :class="$style.title">worota</div>
+            <div :class="$style.buttons">
+              <button :class="$style.hbutton" @click="login()">ログイン</button>
+              <button :class="$style.hbutton" @click="route_post()">お題投稿</button>
             </div>
         </header>
     </div>
@@ -58,8 +58,8 @@ export default {
 }
 </script>
 
-<style>
-header {
+<style lang="scss" module>
+.header {
   width: 100vw;
   height:6vh;
   background: white 0% 0% no-repeat padding-box;
